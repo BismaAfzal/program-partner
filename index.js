441f5bd02bfa191a9709687e95e375fe71add567
Bisma Afzal <iamrose790@gmail.com>
2025-10-29 21:45:28 +0500

    getting know about variable and condition statement in JS

---
+++ index.js
@@ -0,0 +1,12 @@
+const name = "Bisma";
+let secname = true;
+if( name == "Sarfraz"){
+   console.log("name is mylove")
+}
+else if( name == "Bisma"){
+    console.log("name is mine")
+}
+else{
+    console.log("nothing")
+}
+name == "Bisma" ? console.log("name is true"):console.log("name is false")
