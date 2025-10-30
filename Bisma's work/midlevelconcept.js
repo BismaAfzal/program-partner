e0e00558cea0a9ab2b78d2073dd66b9d4c3edcd4
Bisma Afzal <iamrose790@gmail.com>
2025-10-30 21:56:36 +0500

    learned about array, object and function

---
+++ Bisma's work/midlevelconcept.js
@@ -0,0 +1,22 @@
+let finalnum = [2,3,5,5,87,11,0,455, 10];
+ 
+
+
+let result = {
+    Database:{ mid:"20",final:"45"},
+    IS: "24",
+    DSA: "25",
+    multi:"25"
+}
+console.log(result.Database.final)
+
+function pusharr() {
+    finalnum.push(155)
+    console.log(finalnum )
+}
+pusharr()
+
+const pusharr1 = () =>{
+    console.log("bisma")
+}
+pusharr1()
