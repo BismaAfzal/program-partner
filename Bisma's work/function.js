7b56ead481aa09e5baa9e3a9dc81fd858ea77ccb
Bisma Afzal <iamrose790@gmail.com>
2026-02-02 12:47:09 +0500

    To find count number of vowel in a string

--- Bisma's work/function.js
+++ Bisma's work/function.js
@@ -1,11 +1,25 @@
 // write a javascript function that return array elements larger than a number
-let arr = [2,5,10,6,9]
-let n = 5
-function larger(arr, n){
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]>n){
-            console.log(arr[i])
+// let arr = [2,5,10,6,9]
+// let n = 5
+// function larger(arr, n){
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i]>n){
+//             console.log(arr[i])
+//         }
+//     }
+// larger(arr,n);
+// write a JS function count the number of vowel in a string argument?
+let str = "BismaAfzaal";
+function countvowel(str){
+    let count = 0;
+    for(let i=0; i<str.length; i++){
+        if(
+            str.charAt(i) == "a"||str.charAt(i) == "e"||
+            str.charAt(i) == "i" ||str.charAt(i) == "o" ||
+            str.charAt(i) == "u"
+        ){
+            count++
         }
-    }
+    } return count;
 }
-larger(arr,n);
+console.log(countvowel(str))
